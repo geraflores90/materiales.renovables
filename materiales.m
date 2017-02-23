c89b3270bbd32442f88016b7c7675d57cc268cea
geraflores90 <geraflores90@gmail.com>
2017-02-23 12:31:46 -0600

    Add files via upload

--- materiales.m
+++ materiales.m
@@ -1,59 +1,72 @@
-% Calcula la energia solar recibida en un dia a una latitud y dia 
-% especifico.
+% Grafica la energia solar recibida en un año a una latitud especifica.
 
 clear
 clc
 
 phi=19; % Latitud
-n=63; % Dia especifico del año
+%n=63; % Dia especifico del año
 dia=[];
 delta=[];
 matN=[];
 h=[];
+Eanual=0;
+matE=[];
+mat_dias=[];
 
-for i=1:365 
-    dia=[dia;i];
-    op_delta=23.5*sind((360/365)*(284+i));
-    delta=[delta;op_delta];
-    op_N=(2/15)*acosd(-tand(phi)*tand(delta(i)));
-    matN=[matN;op_N];
-    op_h=asind(sind(delta(i))*sind(phi)+cosd(delta(i))*cosd(phi)*cosd(0));
-    h=[h;op_h];
-end
+for n=1:365
+    for i=1:365 
+        dia=[dia;i];
+        op_delta=23.5*sind((360/365)*(284+i));
+        delta=[delta;op_delta];
+        op_N=(2/15)*acosd(-tand(phi)*tand(delta(i)));
+        matN=[matN;op_N];
+        op_h=asind(sind(delta(i))*sind(phi)+cosd(delta(i))*cosd(phi)*cosd(0));
+        h=[h;op_h];
+    end
 
-N=matN(n);
-H=(1/4)*(N*60);
-H=round(H);
-matH=[H];
-I=1000*sind(asind(sind(delta(n))*sind(phi)+cosd(delta(n))*cosd(phi)*cosd(H)));
-matI=[I];
-horas=12-(H*4/60);
-mat_horas=[horas];
-
-for i=1:364
-    H=H-1;
-    matH=[matH;H];
+    N=matN(n);
+    H=(1/4)*(N*60);
+    H=round(H);
+    matH=[H];
     I=1000*sind(asind(sind(delta(n))*sind(phi)+cosd(delta(n))*cosd(phi)*cosd(H)));
-    matI=[matI;I];
+    matI=[I];
     horas=12-(H*4/60);
-    mat_horas=[mat_horas;horas];
+    mat_horas=[horas];
+
+    for i=1:364
+        H=H-1;
+        matH=[matH;H];
+        I=1000*sind(asind(sind(delta(n))*sind(phi)+cosd(delta(n))*cosd(phi)*cosd(H)));
+        matI=[matI;I];
+        horas=12-(H*4/60);
+        mat_horas=[mat_horas;horas];
+    end
+
+    graf=horzcat(mat_horas,matI);
+    valores=[]; % Grafica que almacenara los valores a integrar (valores del 
+                % eje y superiores a cero.
+
+    for i=1:365
+        if graf(i,2)>=0
+            valor=graf(i,:);
+            valores=[valores;valor];
+        end        
+    end
+
+    valores;
+    %plot(valores(:,1),valores(:,2),'o','markersize',4)
+
+    E=trapz(valores(:,1),valores(:,2));
+    Eanual=Eanual+E;
+    matE=[matE;E]; 
+    mat_dias=[mat_dias;n];
 end
 
-graf=horzcat(mat_horas,matI);
-valores=[]; % Grafica que almacenara los valores a integrar (valores del 
-            % eje y superiores a cero.
-
-for i=1:365
-    if graf(i,2)>=0
-        valor=graf(i,:);
-        valores=[valores;valor];
-    end        
-end
-
-valores;
-plot(valores(:,1),valores(:,2),'o','markersize',4)
-    
-E=trapz(valores(:,1),valores(:,2)) 
+graf2=horzcat(mat_dias,matE);
+plot(graf2(:,1),graf2(:,2),'o','markersize',4)
+title('E anual a latitud =19°')
+xlabel('Día')
+ylabel('E')
 
 
     
